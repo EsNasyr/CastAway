b7fc4789a4b0e745b6107b419b6117d01ce3f3de
Eren-Yeag <esenbaevnasyr@gmail.com>
2022-05-24 19:34:44 +0300

    footer-styled

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import homeImg2 from "../../Images/Frame.svg";
-
+import frame from "../../Images/Frame1.png"
 const Footer = () => {
     return (
         <footer>
             <div className="footerWrapper">
                 <div className="">
-                    <h2>Castaway</h2>
+                    <h2 className="footerTitle">Castaway</h2>
+                    <img src={frame} alt=""/>
                 </div>
                 <ul>
-                    <li><a href="" className="footerLink">Home</a></li>
-                    <li><a href="" className="footerLink">About</a></li>
-                    <li><a href="" className="footerLink">Episodes</a></li>
-                    <li><a href="" className="footerLink">Contact</a></li>
+                    <li><a href="#home" className="footerLink">Home</a></li>
+                    <li><a href="#about" className="footerLink">About</a></li>
+                    <li><a href="#episodes" className="footerLink">Episodes</a></li>
+                    <li><a href="#contact" className="footerLink">Contact</a></li>
                 </ul>
                 <ul>
                     <li className="footerLink">Style Guide</li>
@@ -22,7 +23,7 @@ const Footer = () => {
                     <li className="footerLink">Powered by WebfLow</li>
                     <li className="footerLink">Licenses</li>
                 </ul>
-                <img src={homeImg2} alt=""/>
+                <img src={homeImg2} className="footerImg" alt=""/>
             </div>
         </footer>
     );
