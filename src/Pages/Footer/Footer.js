390f36f83a4b6c1d92e2c22d0322e24f53844ce9
Eren-Yeag <esenbaevnasyr@gmail.com>
2022-05-24 18:35:33 +0300

    added footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -9,18 +9,18 @@ const Footer = () => {
                     <h2>Castaway</h2>
                 </div>
                 <ul>
-                    <li><a href="" className="footerLink"></a></li>
-                    <li><a href="" className="footerLink"></a></li>
-                    <li><a href="" className="footerLink"></a></li>
-                    <li><a href="" className="footerLink"></a></li>
+                    <li><a href="" className="footerLink">Home</a></li>
+                    <li><a href="" className="footerLink">About</a></li>
+                    <li><a href="" className="footerLink">Episodes</a></li>
+                    <li><a href="" className="footerLink">Contact</a></li>
                 </ul>
                 <ul>
-                    <li className="footerLink"></li>
-                    <li className="footerLink"></li>
-                    <li className="footerLink"></li>
-                    <li className="footerLink"></li>
-                    <li className="footerLink"></li>
-                    <li className="footerLink"></li>
+                    <li className="footerLink">Style Guide</li>
+                    <li className="footerLink">Instructions</li>
+                    <li className="footerLink">Changelog</li>
+                    <li className="footerLink">Credit</li>
+                    <li className="footerLink">Powered by WebfLow</li>
+                    <li className="footerLink">Licenses</li>
                 </ul>
                 <img src={homeImg2} alt=""/>
             </div>
